4e1d06f1ff1c94d73905b18e0acabbe03dd4e3f1
Andres Franco <andresfranla@gmail.com>
2024-01-17 14:33:18 -0600

    adding the funtions for the cart

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ShopContextProvider } from './context/shop-context.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <ShopContextProvider>
+    <React.StrictMode>
     <App />
-  </React.StrictMode>,
+    </React.StrictMode>
+  </ShopContextProvider>,
 )
